4fc0b75f79aa6a6820cfbfd38ca98bf4e8c0a9a1
Adrian von Gegerfelt <adrian@designbyadrian.com>
2015-03-13 10:58:18 +0000

    made van gogh look nice

--- vangogh/vangogh.js
+++ vangogh/vangogh.js
@@ -1,13 +1,10 @@
-var setup = function(){
+var bar, busy = false, btnSave, c;
+	setup = function(url){
 
-	var testURL = "img/image.jpg",
-		progress = document.getElementById('bar'),
-		btnSave = document.getElementById('btnSave'),
-		c = document.getElementById('eazel'),
-		context = c.getContext('2d'),
+	var context = c.getContext('2d'),
 		img = new Image(),
 		w, h, chunkSize = 10, // multiple of 10
-		worker = new Worker('worker.js'),
+		worker = new Worker('worker.js');
 		draw = function(points){
 
 			for(var i = 0, ii = points.length; i < ii; i++) {
@@ -25,52 +22,96 @@ var setup = function(){
 				context.lineCap = 'round';
 				context.lineTo(x1+x2, y1+y2);
 				context.stroke();
-
 			}
-
 		},
 		start = function(){
 			c.width = w = img.width;
 			c.height = h = img.height;
 			context.drawImage(this, 0, 0);
 
-			console.info("Starting");
+			bar.addClass('active').css('width',0);
+			btnSave.addClass('disabled');
 
 			var pixels = context.getImageData(0, 0, w, h);
 
-			worker.postMessage('{
+			worker.postMessage({
 				'status': 		'start',
 				'pixels': 		pixels.data,
 				'chunkSize': 	chunkSize,
 				'width': 		w,
 				'height': 		h
-			}');
-		},
-		dlCanvas = function() {
-			var dt = c.toDataURL('image/png');
-			this.href = dt.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
-			console.log("dl!",this.href);
+			});
 		};
 
 	worker.onmessage = function(e) {
 
 		switch(e.data.status) {
 			case 'progress':
-				progress.setAttribute('style','width:'+e.data.progress+"%");
+				bar.css('width',e.data.progress+"%");
 				break;
 			case 'done':
-				console.info("Done! :D",e.data);
+				bar.removeClass('active');
 				context.clearRect ( 0 , 0 , c.width, c.height );
 				draw(e.data.points);
-				btnSave.className = btnSave.className.replace(/\bdisabled\b/,'');
+				btnSave.removeClass('disabled');
+				busy = false;
+				worker.terminate();
 				break;
 		}
-
-		
 	}
 
-	btnSave.addEventListener('click', dlCanvas, false);
-
 	img.onload = start;
-	img.src = testURL;
+	img.src = url;
+},
+dlCanvas = function() {
+	var dt = c.toDataURL('image/png');
+	this.href = dt.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+},
+uploadResult = function(result){
+	if(parseInt(result,10)<0) {
+		console.error("Got error "+result);
+
+		switch(parseInt(result,10)) {
+			case -2:
+				alert("Upload problem of some sort. Please try again.");
+				break;
+			case -3:
+				alert("Sorry, image is too large! Please upload an image under 3 MB");
+				break;
+			case -4:
+				alert("Please upload a proper image file!");
+				break;
+			case -5:
+				alert("Could not store image because server is dumb.");
+				break;
+			case -1:
+			default:
+				alert("Something went wrong, and I have no idea what it was.");
+		}
+		busy = false;
+
+	} else {
+		setup('../uploader/data/'+result);
+	}
 };
+
+$(function(){
+
+	bar = $("#bar");
+	btnSave = $("#btnSave");
+
+	c = document.getElementById('eazel');
+
+	btnSave.click(dlCanvas);
+
+	$("#file").on('click',function(e){
+		if(busy) {
+			return false;
+		} else {
+			busy = true;
+		}
+	}).change(function(){
+		$("#form").submit();
+	});
+
+});
